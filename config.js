387e2d5efad80d4bbd1f4d43112e8f941be1cf66
qw40 <Tchabal509@gmail.com>
2025-02-21 12:03:02 -0400

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
     autoRead: process.env.AUTO_READ === "true",
     autoViewStatus: process.env.AUTO_VIEW_STATUS === "true",
     autoReact: process.env.AUTO_REACT === "true",
-    sessionId: process.env.SESSION_ID || "",
+    sessionId: process.env.SESSION_ID || "KHAN-MD~7u4n0CpY#giR9ntjvIDbLpAz8IpRLubYSWlIfwlbk1D8kzrlrOmU",
     autoRejectEnabled: process.env.AUTO_REJECT_ENABLED || "false",
 };
 
